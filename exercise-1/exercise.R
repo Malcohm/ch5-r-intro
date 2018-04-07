d55d4e7a5f86d81b062ddd8986a38eef78e40bd1
Malcohm <malcohm@uw.edu>
2018-04-07 15:36:22 -0700

    add variables corresponding to exercise questions

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,25 +2,35 @@
 
 # Create a variable `hometown` that stores the city in which you were born
 
+> hometown <- "Brunei" # hometown refers to the city I was born in
 
 # Assign your name to the variable `my_name`
 
+> my_name <- "Malcohm" # refers to my name
 
 # Assign your height (in inches) to a variable `my_height`
 
+> my_height <- 68.4 # refers to my height in inches
 
 # Create a variable `puppies` equal to the number of puppies you'd like to have
 
+> puppies <- 1 # puppies refers to the number of puppies I'd like to have
 
 # Create a variable `puppy_price`, which is how much you think a puppy costs
 
+> puppy_price <- 300 # refers to cost of one puppy
 
 # Create a variable `total_cost` that has the total cost of all of your puppies
 
+> total_cost <- 300 # refers to the total cost of puppies
 
 # Create a boolean variable `too_expensive`, set to TRUE if the cost is greater 
 # than $1,000
 
+> too_expensive <- 1000 # refers to if the cost of puppies are greater than 1000
+> total_cost > too_expensive #returns logical value FALSE (total_cost is not bigger than too_expensive)
 
 # Create a variable `max_puppies`, which is the number of puppies you can 
 # afford for $1,000
+
+> max_puppies <- 3 #refers to the number of puppies I can afford for $1,000
